f0c60c5678d7e6f79f3105dce3619758db46482d
王超 <wangchaoit@qq.com>
2023-09-08 16:50:11 +0800

    修改参数命名

--- EventBus/IRequest.cs
+++ EventBus/IRequest.cs
@@ -7,33 +7,33 @@
     /// <summary>
     /// 事件总线处理程序接口
     /// </summary>
-    /// <typeparam name="TIn"></typeparam>
-    public interface IRequestHandler<TIn> where TIn : IRequest
+    /// <typeparam name="TRequest"></typeparam>
+    public interface IRequestHandler<TRequest> where TRequest : IRequest
     {
         /// <summary>
         /// 执行对象处理方法
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        Task HandleAsync(TIn request);
+        Task HandleAsync(TRequest request);
     }
     /// <summary>
     /// 事件总线请求对象接口（带返回参数）
     /// </summary>
-    /// <typeparam name="TOut"></typeparam>
-    public interface IRequest<TOut> { }
+    /// <typeparam name="TResponse"></typeparam>
+    public interface IRequest<TResponse> { }
     /// <summary>
     /// 事件总线处理程序接口（带返回参数）
     /// </summary>
-    /// <typeparam name="TIn"></typeparam>
-    /// <typeparam name="TOut"></typeparam>
-    public interface IRequestHandler<TIn, TOut> where TIn : IRequest<TOut>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
         /// <summary>
         /// 执行对象处理方法
         /// </summary>
         /// <param name="request">返回处理结果</param>
         /// <returns></returns>
-        Task<TOut> HandleAsync(TIn request);
+        Task<TResponse> HandleAsync(TRequest request);
     }
 }
