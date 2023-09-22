87d34a74decb61a6dd72cd55fdf63a1aaa613b2d
王超 <wangchaoit@qq.com>
2023-09-22 17:15:08 +0800

    删除不需要的引用

--- EventBus/ISimpleEventBus.cs
+++ EventBus/ISimpleEventBus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleEventBus
+﻿namespace SimpleEventBus
 {
     /// <summary>
     /// 事件总线接口
